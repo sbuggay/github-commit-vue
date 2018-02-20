acb3a04a350864f20405652cb621c6cf97d3ef7d
Devan Buggay <sbuggay@gmail.com>
2018-02-19 21:57:49 -0800

    initial api commit

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import * as express from "express";
+import * as octokat from "octokat";
+const app = express();
+
+const USER = "sbuggay";
+const EMAIL = "sbuggay@gmail.com";
+
+const github = octokat({ token: "fe886d4658de8f66b482ed9a4f49fc9f8d1d6beb" });
+
+function getCommits() {
+    return github.fromUrl(`https://api.github.com/users/${USER}/events`).fetch().then(events => {
+        const commits = [];
+        // console.log(events);
+        events.items.forEach(event => {
+            if (event.type === "PushEvent") {
+                event.payload.commits.reverse().forEach(commit => {
+                    if (commit.author.email === EMAIL) {
+                        commits.push({
+                            repo: event.repo,
+                            createdAt: event.createdat,
+                            ...commit
+                        });
+                    }
+                });
+            };
+        });
+        console.log(commits);
+        return commits;
+    }, failure => {
+        console.error(failure);
+    });
+}
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", (req, res) => {
+    res.send('hello world');
+});
+
+app.get("/api", (req, res) => {
+    getCommits().then(commits => {
+        res.send(commits);
+    });
+});
+
+app.listen(3030, () => console.log(`app started`));
