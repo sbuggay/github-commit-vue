03269f26fdd0a494a218fb9e6db20638d902adfc
Devan Buggay <debuggay@microsoft.com>
2018-03-05 11:53:14 -0800

    Fix update interval

--- public/script.js
+++ public/script.js
@@ -2,15 +2,14 @@ const app = new Vue({
     el: "#vue",
     data: {
         commits: [],
-        refresh: 60000 // refresh time in ms
+        refresh: 60 * 1000 // refresh time in ms (1 minute)
     },
     created() {
         this.fetchCommits();
-        setTimeout(() => this.fetchCommits, this.refresh);
+        const interval = setInterval(() => this.fetchCommits(), this.refresh);
     },
     methods: {
         async fetchCommits() {
-            console.log("refresh");
             const response = await fetch("/api");
             this.commits = await response.json();
         }
